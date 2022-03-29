3077ca6d34ffd49dc4490fb0bf96ebebc4f62ec3
AreyBRSK <areybrsk@gmail.com>
2022-03-29 11:41:00 +0300

    User model configuration

--- AboutUser/Models/Models.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Models.swift
-//  AboutUser
-//
-//  Created by Андрей Барсук on 29.03.2022.
-//
-
-import Foundation
---
+++ AboutUser/Models/User.swift
@@ -0,0 +1,51 @@
+//
+//  Models.swift
+//  AboutUser
+//
+//  Created by Андрей Барсук on 29.03.2022.
+//
+
+struct User {
+  let username: String
+  let password: String
+  var userProfile: UserProfile
+}
+
+struct UserProfile {
+  let firstName: String
+  let lastName: String
+  var displayName: String
+  var about: UserAbout
+  let contacts: UserContacts
+}
+
+struct UserAbout {
+  let dateOfBirth: String
+  let aboutText: String
+  let favoriteAnimal: String
+  let favoriteBook: String
+}
+
+struct UserContacts {
+  let phoneNumber: String
+  let email: String
+}
+
+var defaultUser = User(
+  username: "username",
+  password: "password",
+  userProfile: UserProfile(
+    firstName: "Andrey",
+    lastName: "Barsuk",
+    displayName: "ABRSK",
+    about: UserAbout(
+      dateOfBirth: "March 5th, 1990",
+      aboutText: "About text goes here...",
+      favoriteAnimal: "Cats",
+      favoriteBook: "The Master and His Emissary by Iain McGilchrist"),
+    contacts: UserContacts(
+      phoneNumber: "+7 994 417 XX XX",
+      email: "xxxxxxxx@gmail.com"
+    )
+  )
+)
