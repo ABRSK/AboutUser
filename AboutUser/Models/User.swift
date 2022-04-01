61e4a26d6e42b83c68be391a99aba0f6798aac6c
AreyBRSK <areybrsk@gmail.com>
2022-04-01 09:09:50 +0300

    Moved global property to static within model

--- AboutUser/Models/User.swift
+++ AboutUser/Models/User.swift
@@ -9,6 +9,25 @@ struct User {
   let username: String
   let password: String
   let userProfile: UserProfile
+  
+  static let defaultUser = User(
+    username: "username",
+    password: "password",
+    userProfile: UserProfile(
+      firstName: "Andrey",
+      lastName: "Barsuk",
+      displayName: "ABRSK",
+      about: UserAbout(
+        dateOfBirth: "March 5th, 1990",
+        aboutText: "Nulla facilisi. Quisque tempor odio vel feugiat semper. Phasellus purus justo, sagittis at pellentesque at, lobortis faucibus enim. Quisque dignissim cursus magna, vestibulum congue turpis laoreet quis. ",
+        favoriteAnimal: "Cat",
+        favoriteBook: "The Master and His Emissary"),
+      contacts: UserContacts(
+        phoneNumber: "+7 994 417 XX XX",
+        email: "xxxxxxxx@gmail.com"
+      )
+    )
+  )
 }
 
 struct UserProfile {
@@ -31,23 +50,3 @@ struct UserContacts {
   let email: String
 }
 
-// Default user
-
-let defaultUser = User(
-  username: "username",
-  password: "password",
-  userProfile: UserProfile(
-    firstName: "Andrey",
-    lastName: "Barsuk",
-    displayName: "ABRSK",
-    about: UserAbout(
-      dateOfBirth: "March 5th, 1990",
-      aboutText: "Nulla facilisi. Quisque tempor odio vel feugiat semper. Phasellus purus justo, sagittis at pellentesque at, lobortis faucibus enim. Quisque dignissim cursus magna, vestibulum congue turpis laoreet quis. ",
-      favoriteAnimal: "Cat",
-      favoriteBook: "The Master and His Emissary"),
-    contacts: UserContacts(
-      phoneNumber: "+7 994 417 XX XX",
-      email: "xxxxxxxx@gmail.com"
-    )
-  )
-)
